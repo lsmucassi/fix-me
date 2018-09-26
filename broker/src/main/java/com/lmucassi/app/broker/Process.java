f4c22294153ad736d835d514ae8d1defc435c0bc
Linda MUCASSI <lmucassi@e4r8p6.wethinkcode.co.za>
2018-09-26 16:08:20 +0200

    Do :fix mes - broker

--- broker/src/main/java/com/lmucassi/app/broker/Process.java
+++ broker/src/main/java/com/lmucassi/app/broker/Process.java
@@ -4,18 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Process {
-    public Process() {
+    private String _inst;
+    private int _quant;
+    private String _markt;
+    private int _price;
+    Map<String, String> fxMess = new HashMap<String, String>();
 
+    public Process(String inst, int quant, String markt, int price) {
+        this._inst = inst;
+        this._quant = quant;
+        this._markt = markt;
+        this._price = price;
     }
 
-    public void getFXMess(String inst, String quant, String markt, String price){
-        Map<String, String> fxMess = new HashMap<String, String>();
-        fxMess.put("Instrument", inst);
-        fxMess.put("Quantity", quant);
-        fxMess.put("Market", markt);
-        fxMess.put("Price", price);
+    public void showFXMess() {
+        fxMess.put("Instrument", this._inst);
+        fxMess.put("Quantity", Integer.toString(this._quant));
+        fxMess.put("Market", this._markt);
+        fxMess.put("Price", Integer.toString(this._price));
 
+        System.out.println("using entrySet and toString");
+        for (Map.Entry<String, String> entry : fxMess.entrySet()) {
+            System.out.println(entry);
+        }
+        System.out.println();
     }
-
 
 }
